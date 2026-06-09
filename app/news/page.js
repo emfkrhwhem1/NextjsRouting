d68d5c66da93e071284848488b9f6fc7e87dbb91
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-06-09 11:58:39 +0900

    NotFound 페이지 , GitIgnore 파일 추가

---
+++ app/news/page.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+export default function NewsPage() {
+    return (
+        <>
+            <h1>News Page</h1>
+            <ul className="news-list">
+                {DUMMY_NEWS.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <Link href={`/news/${item.slug}`}>
+                                <img src={`/images/news/${item.image}`} alt={item.title} />
+                                <span>{item.title}</span>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    );
+}
