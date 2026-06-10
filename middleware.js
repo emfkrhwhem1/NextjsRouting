9042b44638d56958f0c0fd61273bd7147502cf69
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-06-10 20:57:55 +0900

    middleware 배움

--- middleware.js
+++ middleware.js
@@ -4,3 +4,8 @@ export default function middleware(request) {
     console.log(request);
     return NextResponse.next();
 }
+
+
+export const config = {
+    matcher: '/news', // 보고싶은 request만 보는 필터링 기능 
+}
